c31ff99d5e0d024b861cd3819b550e5e8189cc7d
Lucas Gomes <lucasggomees@gmail.com>
2025-03-30 23:41:44 +0100

    chore: setup project configuration with Husky, Prettier, and ESLint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "breaking",
+      ],
+    ],
+    "header-max-length": [2, "always", 150],
+  },
+}
+
+export default config
