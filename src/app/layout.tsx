8465aedcd1b010d2f0b20312bccc54e816ac33d5
Lucas Gomes <lucasggomees@gmail.com>
2025-04-03 23:34:32 +0100

    feat: add dashboard page with invoice table and UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-dvh`}
-      >
+      <body className="flex flex-col max-w-5xl mx-auto">
         <header>Header</header>
-        <main className="">{children}</main>
+        <main className="flex-1 my-12">{children}</main>
         <footer>Footer</footer>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-6 justify-center max-w-5xl mx-auto">
+    <div className="flex flex-col gap-6 justify-center h-full text-center">
       <h1 className="text-5xl font-bold">PayMate</h1>
       <p>
         <Button asChild>
